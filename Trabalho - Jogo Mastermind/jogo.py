97b900fbe7cfa85ef81fe837530f1341d9061874
Rafaela Carneiro <94813140+rafaelamscarneiro@users.noreply.github.com>
2022-03-19 11:20:50 -0300

    Add files via upload

---
+++ Trabalho - Jogo Mastermind/jogo.py
@@ -0,0 +1,11 @@
+'''
+Autores:    Daniel Peralta (1811442)
+            Felipe Meiga (2011460)
+            Rafaela Carneiro (2011483)
+'''
+from view import desenha_canvas as draw
+from model import regras_do_jogo as rules
+from controller import seq_eventos as event 
+
+event.janela_inicial() # abre a janela inicial do jogo
+
